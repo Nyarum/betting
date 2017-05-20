8a7a76466a4d8e7520e2df9625ea66b666750fac
kalamadea <JohnDoe@mail.com>
2017-05-20 16:37:36 +0300

    Add ListVenueResult #5

--- betting_list_test.go
+++ betting_list_test.go
@@ -180,7 +180,7 @@ func TestRequestListMarketProfitAndLoss(t *testing.T) {
 	}
 
 	list, err := bet.ListMarketProfitAndLoss(Filter{
-		MarketProfitAndLossMarketIds: []string{"1.22272098"},
+		MarketProfitAndLossMarketIds: []string{"1.125821194"},
 	})
 	if err != nil {
 		t.Error(err)
@@ -202,7 +202,7 @@ func TestRequestListMarketTypes(t *testing.T) {
 	}
 
 	list, err := bet.ListMarketTypes(Filter{
-		MarketFilter: &MarketFilter{MarketIDs: []string{"1.22272098"}},
+		MarketFilter: &MarketFilter{MarketIDs: []string{"1.125821194"}},
 		Locale:       "en",
 	})
 	if err != nil {
@@ -225,7 +225,7 @@ func TestRequestListTimeRangeResult(t *testing.T) {
 	}
 
 	list, err := bet.ListTimeRangeResult(Filter{
-		MarketFilter:    &MarketFilter{MarketIDs: []string{"1"}},
+		MarketFilter:    &MarketFilter{MarketIDs: []string{"1.125821194"}},
 		TimeGranularity: TG_DAYS,
 	})
 	if err != nil {
@@ -234,5 +234,28 @@ func TestRequestListTimeRangeResult(t *testing.T) {
 
 	if config.Debug {
 		log.Printf("TestRequestListTimeRangeResult\n %v \n\n", list)
+	}
+}
+
+func TestRequestListVenueResult(t *testing.T) {
+	config := loadConfig()
+
+	bet := NewBet(config.ApiKey)
+
+	err := bet.GetSession(config.CertPem, config.CertKey, config.Login, config.Password)
+	if err != nil {
+		t.Error(err)
+	}
+
+	list, err := bet.ListVenueResult(Filter{
+		MarketFilter: &MarketFilter{MarketIDs: []string{"1.125821194"}},
+		Locale:       "en",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+
+	if config.Debug {
+		log.Printf("TestRequestListVenueResult\n %v \n\n", list)
 	}
 }
